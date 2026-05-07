aedbcfb051bc6babd07b75541bf7ee178b8c5ad2
Madhav Mishra <madhavmishra083@gmail.com>
2026-05-07 13:27:50 +0530

    feat: adding login system

---
+++ frontend/authentication/login.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+function Login(){
+    const [formData, setFormData] = useState({
+        email: "",
+        password: "",
+    });
+    const handleChange=(e)=>{
+        const {name,value} =e.target;
+        setFormData({
+            ...formData,
+            [name]: value
+        });
+    };
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+        console.log("Login Data:", formData)
+    };
+    return(
+        <>
+            <div className="font-roboto">
+                <form onSubmit={handleSubmit}>
+                    <h2>Login</h2>
+                    <label htmlFor="emailID">Email Address</label>
+                    <input type="email" name="email" id="email" placeholder="Enter Email" value={formData.email} onChange={handleChange} required />
+                    <label htmlFor="password">Password</label>
+                    <input type="password" name="password" id="password" placeholder="Enter Password" value={formData.password} onChange={handleChange} required />
+                    <button type="submit">Login</button>
+                </form>
+            </div>
+        </>
+    )
+}
