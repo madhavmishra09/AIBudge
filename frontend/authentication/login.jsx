486bfa003f21ffee3bb849bb95ab6bc6b77a5ee9
Madhav Mishra <madhavmishra083@gmail.com>
2026-05-08 13:27:38 +0530

    fix: importing react-router-dom instead of react-router

--- frontend/authentication/login.jsx
+++ frontend/authentication/login.jsx
@@ -22,7 +22,7 @@ export default function Login(){
                 <form onSubmit={handleSubmit}>
                     <h2>Login</h2>
                     <label htmlFor="emailID">Email Address</label>
-                    <input type="email" name="email" id="email" placeholder="Enter Email" value={formData.email} onChange={handleChange} required />
+                    <input type="email" name="email" id="emailID" placeholder="Enter Email" value={formData.email} onChange={handleChange} required />
                     <label htmlFor="password">Password</label>
                     <input type="password" name="password" id="password" placeholder="Enter Password" value={formData.password} onChange={handleChange} required />
                     <Link to="/forgot-password">Forgot Password?</Link>
