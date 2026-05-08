d7182732e446c09fc75457dd1addbae686bc1128
Madhav Mishra <madhavmishra083@gmail.com>
2026-05-08 12:09:31 +0530

    build: adding routes to the page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,21 @@
 import { useState } from 'react'
 import './App.css'
-
+import LandingPage from '../pages/landingpage'
+import NavigationBar from '../components/navbar'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <></>
+    <>
+      <Routes>
+        <Route path="/" element={
+          <>
+            <NavigationBar />
+            <LandingPage />
+          </>
+        } />
+      </Routes>
+    </>
   )
 }
 
