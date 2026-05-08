7418ed282257d648eaf705385d56c58639dce9dc
Madhav Mishra <madhavmishra083@gmail.com>
2026-05-08 12:23:42 +0530

    feat: Authentication, Landing Page and Navigation Bar routes are added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,9 @@
 import { useState } from 'react'
+import {Routes, Route} from 'react-router'
 import './App.css'
+import Login from '../authentication/login'
+import SignUp from '../authentication/signup'
+import ForgotPassword from '../authentication/forgotpassword'
 import LandingPage from '../pages/landingpage'
 import NavigationBar from '../components/navbar'
 function App() {
@@ -14,6 +18,9 @@ function App() {
             <LandingPage />
           </>
         } />
+        <Route path="/login" element={<Login/>}/>
+        <Route path="/signup" element={<SignUp/>}/>
+        <Route path="/forgot-password" element={<NavigationBar/>}/>
       </Routes>
     </>
   )
