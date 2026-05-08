19bdd1236d54b30822a447940693d31dd1f92d66
Madhav Mishra <madhavmishra083@gmail.com>
2026-05-08 23:07:55 +0530

    feat: added routing to spend input form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
 import {Routes, Route} from 'react-router-dom'
 import './App.css'
 import Login from '../authentication/login'
 import SignUp from '../authentication/signup'
 import ForgotPassword from '../authentication/forgotpassword'
 import LandingPage from '../pages/landingpage'
+import SpendInputForm from '../pages/spendinputform'
 import NavigationBar from '../components/navbar'
 function App() {
   return (
@@ -12,6 +12,7 @@ function App() {
       <NavigationBar />
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/spend-input" element={<SpendInputForm />} />
         <Route path="/login" element={<Login/>}/>
         <Route path="/signup" element={<SignUp/>}/>
         <Route path="/forgot-password" element={<ForgotPassword/>}/>
