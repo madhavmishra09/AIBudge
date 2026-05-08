fc41abcfb3315a7a0d9f642062cd679725c7e1e7
Madhav Mishra <madhavmishra083@gmail.com>
2026-05-08 13:44:04 +0530

    fix: Routing has been fixed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import {Routes, Route} from 'react-router'
+import {Routes, Route} from 'react-router-dom'
 import './App.css'
 import Login from '../authentication/login'
 import SignUp from '../authentication/signup'
@@ -7,20 +7,14 @@ import ForgotPassword from '../authentication/forgotpassword'
 import LandingPage from '../pages/landingpage'
 import NavigationBar from '../components/navbar'
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
+      <NavigationBar />
       <Routes>
-        <Route path="/" element={
-          <>
-            <NavigationBar />
-            <LandingPage />
-          </>
-        } />
+        <Route path="/" element={<LandingPage />} />
         <Route path="/login" element={<Login/>}/>
         <Route path="/signup" element={<SignUp/>}/>
-        <Route path="/forgot-password" element={<NavigationBar/>}/>
+        <Route path="/forgot-password" element={<ForgotPassword/>}/>
       </Routes>
     </>
   )
