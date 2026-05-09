a3f66322caed2f14f13e2caf711b7cb159edee1e
Madhav Mishra <madhavmishra083@gmail.com>
2026-05-09 23:58:01 +0530

    feat: added routing for forms

---
+++ backend/routes/formRoutes.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const {
+  submitForm,
+} = require("../controllers/formController");
+
+const router = express.Router();
+
+router.post("/submit", submitForm);
+
+module.exports = router;
